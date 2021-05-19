7b8e7589b87792feacb5d4fa7036fb742ca1d707
suyashpradhan <pradhansuyash39@gmail.com>
2021-05-19 22:28:44 +0530

    feat: started project added tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./assets/css/main.css"
+
+export const App = () => {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./assets/css/main.css"
+import {App} from './App';
+import { BrowserRouter as Router } from "react-router-dom"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
